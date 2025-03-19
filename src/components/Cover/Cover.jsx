c257bab7a539b8465fb18f817bf2999aa5b9ad8b
Sisindri Singamsetti <sisindrisingamsetti@gmail.com>
2025-03-19 23:01:33 +0530

    sevas section updated on mobile

--- src/components/Cover/Cover.jsx
+++ src/components/Cover/Cover.jsx
@@ -10,17 +10,17 @@ const Cover = () => {
         <Waves className="z-[-1]" />
         <div
           id="home"
-          className="w-full flex flex-col-reverse md:flex-row items-center  justify-evenly align-center my-auto"
+          className="w-full flex flex-col-reverse md:flex-row items-center  justify-evenly align-center my-auto gap-y-6 md:gap-2"
         >
           <div className="flex flex-col gap-2">
-            <h1 className="text-2xl md:text-5xl font-playFair text-mainColor">
+            <h1 className="text-2xl md:text-5xl font-playFair font-semibold text-mainColor">
               Sri Lakshmi Padmavathi
             </h1>
             <span className="text-center font-montserrat text-white">
               Something here
             </span>
           </div>
-          <img className="w-1/4 rounded-xl" src={bannerPic} alt="" />
+          <img className="w-1/2 md:w-1/4 rounded-xl" src={bannerPic} alt="" />
         </div>
         <img className="m-auto " src={gold} alt="" />
       </div>
--- src/components/Seva/Seva.jsx
+++ src/components/Seva/Seva.jsx
@@ -38,18 +38,18 @@ const Seva = () => {
   ];
 
   return (
-    <div id="seva" className="py-16">
-      <div className="flex flex-col items-center gap-3">
-        <h1 className="text-white ">Sevas</h1>
-        <h1 className="text-xl sm:text-3xl md:text-4xl text-mainColor">
+    <div id="seva" className="py-8 md:py-16">
+      <div className="flex flex-col items-center gap-2 md:gap-3 z-20">
+        <h1 className="text-white text-lg md:text-xl">Sevas</h1>
+        <h1 className="text-lg sm:text-2xl md:text-3xl lg:text-4xl text-mainColor text-center">
           Our Collections
         </h1>
-        <img className="w-1/8 animate-pulse" src={white} alt="" />
+        <img className="w-16 md:w-24 animate-pulse" src={white} alt="" />
       </div>
 
-      <div className="relative flex justify-center items-start gap-6 my-12 w-[90%] m-auto px-4 -mt-36">
+      <div className="relative hidden md:flex flex-row justify-center items-start gap-2 lg:gap-4 xl:gap-6 my-12 w-full md:w-[95%] lg:w-[90%] xl:w-[85%] mx-auto px-4 -mt-36">
         {sevaItems.map((item) => (
-          <div key={item.id} className="flex flex-col items-center">
+          <div key={item.id} className="flex flex-col items-center -z-10">
             <div className="relative my-5">
               <div
                 className="w-[1px] bg-[#B5995A] mx-auto"
@@ -61,11 +61,28 @@ const Seva = () => {
             </div>
 
             <div
+              className={`hover:translate-y-[-12px] h-[280px] lg:h-[320px] xl:h-[350px] w-[180px] lg:w-[230px] xl:w-[280px] ${item.bgClass} bg-cover bg-center relative transition-all duration-[0.3s] ease-in-out p-4 cursor-pointer`}
+            >
+              <div className="text-white absolute bottom-4 p-2">
+                <h1 className="font-montserrat font-bold">{item.title}</h1>
+                <p className="mt-2 font-light text-sm md:text-xs xl:text-sm text-white/80 md:line-clamp-3 lg:line-clamp-4 xl:line-clamp-none">
+                  {item.description}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="relative md:hidden flex flex-col items-center gap-12 my-12 w-full px-4">
+        {sevaItems.map((item) => (
+          <div key={item.id} className="flex flex-col items-center mx-auto">
+            <div
               className={`hover:translate-y-[-12px] h-[350px] w-[280px] ${item.bgClass} bg-cover bg-center relative transition-all duration-[0.3s] ease-in-out p-4 cursor-pointer`}
             >
               <div className="text-white absolute bottom-4 p-2">
                 <h1 className="font-montserrat font-bold">{item.title}</h1>
-                <p className="mt-2 font-light text-sm text-white/80">
+                <p className="mt-2 font-light text-sm md:text-xs xl:text-sm text-white/80">
                   {item.description}
                 </p>
               </div>
