da9acc5ef35966189619d0fb001fb7e4b491ffce
Sisindri Singamsetti <sisindrisingamsetti@gmail.com>
2025-03-31 17:33:09 +0530

    scroll bar updated

--- src/components/Donate/Donate.jsx
+++ src/components/Donate/Donate.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Donate = () => {
   return (
-    <div>
+    <div className="bg-[#FAAC2F] h-auto">
       <h1 className="text-primary text-3xl">Donate</h1>
     </div>
   );
